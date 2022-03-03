57a01e876190309d392825cc80280b4f693d5f12
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-03 20:29:02 +0700

    Make Controller Rev

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class post-detail extends Controller
+class AboutController extends Controller
 {
     //
 }
--- app/Http/Controllers/PostDetailsController.php
+++ app/Http/Controllers/PostDetailsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class about extends Controller
+class PostDetailsController extends Controller
 {
     //
 }
